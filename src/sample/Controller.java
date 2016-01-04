fbf191dc8dee9fc0c13265ee36caa18e7b1ee9d3
rosa <gojiradam@gmail.com>
2016-01-04 21:33:19 +0200

    New building to traceroute and ping, use ICMP via BASH.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,5 @@
 package sample;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -9,34 +8,20 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.net.URI;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Controller implements EventHandler<ActionEvent> {
 
 
     @FXML
-    private TextField address;
+    private Label whoIpLabel, hostname;
     @FXML
-    private Label ip, hostname;
+    public TextArea traceArea , pingArea , whoTextArea;
     @FXML
-    private TextArea whoText;
-    @FXML
-    private TextField traceField;
-    @FXML
-    public TextArea traceArea;
-    @FXML
-    private TextField traceIp;
-    @FXML
-    public TextArea pingArea;
+    private TextField traceIp , whIpField , traceField;
+
 
 
 
@@ -44,15 +29,15 @@ public class Controller implements EventHandler<ActionEvent> {
         whoIs who = new whoIs();
         System.setProperty("apple.laf.useScreenMenuBar", "true");
 
-        String text = address.getText();
+        String text = whIpField.getText();
 
         InetAddress address = null;
         try {
             address = InetAddress.getByName(text);
 
-            ip.setText(address.getCanonicalHostName());
+            whoIpLabel.setText(address.getCanonicalHostName());
             hostname.setText(address.getHostName());
-            whoText.setText(who.getWhois(text));
+            whoTextArea.setText(who.getWhois(text));
 
         } catch (UnknownHostException e) {
             Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -78,26 +63,6 @@ public class Controller implements EventHandler<ActionEvent> {
 
     public void pingButton() {
 
-            Thread t1 = new Thread() {
-
-
-                public void run() {
-
-                    try {
-                        Runtime r = Runtime.getRuntime();
-                        Process p = r.exec("ping " + "8.8.8.8");
-                        BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()));
-                        while ((pingInput = in.readLine()) != null) {
-                            pingArea.appendText(pingInput);
-                            pingArea.appendText("\n");
-                            System.out.println(pingInput);
-                        }
-                        in.close();
-                    } catch (Exception e) {
-                    }
-                }
-            };
-t1.start();
     }
 
     public void link(ActionEvent event) throws Exception {
