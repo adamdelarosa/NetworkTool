ed25ccb8727637a11e6c4ee604e707d5dab05d48
rosa <gojiradam@gmail.com>
2016-01-09 16:13:36 +0200

    TraceRoute Fix

--- src/Network/traceRoute.java
+++ src/Network/traceRoute.java
@@ -1,17 +1,18 @@
 package Network;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.management.ManagementFactory;
 
 public class traceRoute extends Controller {
 
     @FXML
     private Controller textOutTrace, traceButt, traceBar;
+    private String inputLine;
+    private Process runningProcess = null;
+    private Runtime r = null;
+    private BufferedReader in;
+
 
 
     public traceRoute(Controller ta, Controller butt, Controller bar, Controller stop) {
@@ -20,27 +21,24 @@ public class traceRoute extends Controller {
         traceBar = bar;
     }
 
-    public String inputLine;
 
-    public Process runningProcess = null;
-    public Runtime r = null;
     Thread runTrace = new Thread(new Runnable() {
 
         public void run() {
             try {
                 r = Runtime.getRuntime();
                 runningProcess = r.exec("traceroute " + "8.8.8.8");
-                BufferedReader in = new BufferedReader(new InputStreamReader(runningProcess.getInputStream()));
-                String pid = ManagementFactory.getRuntimeMXBean().getName();
+                in = new BufferedReader(new InputStreamReader(runningProcess.getInputStream()));
                 while ((inputLine = in.readLine()) != null) {
-                    System.out.print(pid);
-                    javafx.application.Platform.runLater( () -> textOutTrace.traceArea.appendText(inputLine + "\n") );
+                    javafx.application.Platform.runLater(() -> textOutTrace.traceArea.appendText(inputLine + "\n"));
+
                     traceButt.traceButtonOnAction.setDisable(true);
                     traceBar.traceProgressBar.setVisible(true);
                 }
-
                 traceButt.traceButtonOnAction.setDisable(false);
                 traceBar.traceProgressBar.setVisible(false);
+
+
                 in.close();
             } catch (Exception e) {
             }
@@ -61,18 +59,11 @@ public class traceRoute extends Controller {
     }
 
     public void killTraceRoute() {
-        try {
-            runningProcess = r.exec("kill -9 867");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        BufferedReader in = new BufferedReader(new InputStreamReader(runningProcess.getInputStream()));
-        runningProcess.destroy();
-        runningProcess = null;
+        inputLine = null;
         System.out.print("Stopping");
     }
 }
-
+//Adam
 
 
 
